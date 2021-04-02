8e1c0297a7678c603ab86ef4e85bb8b6ab228e09
sbswarna <79458082+sbswarna@users.noreply.github.com>
2021-04-02 15:12:55 +0600

    solved

---
+++ A. Domino piling.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int m,n;
+void F()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+    #endif 
+}
+int main(int argc, char const *argv[])
+{
+	cin>>n>>m;
+	cout<<(n*m)/2<<endl;
+	return 0;
+}

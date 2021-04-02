0a5fb38e06f93291cf7dd6378074f0d30db82237
sbswarna <79458082+sbswarna@users.noreply.github.com>
2021-04-02 11:55:21 +0600

    solved

---
+++ A. Is it rated - 2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+void F()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+    #endif 
+}
+int main(int argc, char const *argv[])
+{
+	F();
+	while(getline(cin,s))
+	{
+		cout<<"NO"<<endl;
+	}
+	return 0;
+}

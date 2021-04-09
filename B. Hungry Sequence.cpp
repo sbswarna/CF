7513fba8ff2b13d1899ccb2a348367ec3d0275ad
sbswarna <79458082+sbswarna@users.noreply.github.com>
2021-04-09 11:02:04 +0600

    Solved

---
+++ B. Hungry Sequence.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long tst,n;
+void F()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+    #endif 
+}
+int main(int argc, char const *argv[])
+{
+	F();
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		cout<<100000+i<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
